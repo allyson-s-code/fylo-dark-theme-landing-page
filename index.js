a60c1b46ddfa738fcfc6e8ed3b96b591718e24f6
allyson_s <allysonsmithstudio@gmail.com>
2022-07-08 16:53:17 -0400

    Email validation with error message

---
+++ index.js
@@ -0,0 +1,21 @@
+const emailInput = document.getElementById("email");
+const error = document.getElementById("error-message");
+const submit = document.getElementById("email-submit");
+
+//validate email input function
+const mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+const validateEmail = function () {
+  const email = emailInput.value;
+  if (!email || !email.match(mailFormat)) {
+    error.classList.remove("hide");
+  } else {
+    error.classList.add("hide");
+  }
+};
+
+//run validatation on submit
+submit.addEventListener("click", function (e) {
+  e.preventDefault();
+  validateEmail();
+});
